ce4e65fd8731d58203564cd45b18e61da1d9b791
Duncan McRobert <dmcrobert@live.co.uk>
2020-04-01 16:06:38 -0400

    Moved logging out into util file and environment out into util config file. Updated code to call logging and env variables via these new config files

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const config = require('./utils/config')
 const express = require('express')
 const cors = require('cors')
 const app = express()
 const mongoose = require('mongoose')
 const Blog = require('./models/blog')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+logger.info(`Connecting to ${config.MONGODB_URI}`)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(success => (success) ? console.log('Connected To MongoDB Blog Cluster') : '')
-    .catch(error => console.error('Failed to connect to mongoDB Blog cluster:', error.message))
+    .then(() => logger.info('Connected To MongoDB'))
+    .catch(error => logger.error('Failed to connect to mongoDB:', error.message))
 
 app.use(cors())
 app.use(express.json())
