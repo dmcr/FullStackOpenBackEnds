bf9d84403a5e3d6efbe8d8c004da620f39570b59
Duncan McRobert <dmcrobert@live.co.uk>
2020-04-01 18:05:34 -0400

    Added middleware to handle unknown endpoint requests, log requests and handle objectID cast and validation errors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const mongoose = require('mongoose')
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
 const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
 logger.info(`Connecting to ${config.MONGODB_URI}`)
 
@@ -15,6 +15,11 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
