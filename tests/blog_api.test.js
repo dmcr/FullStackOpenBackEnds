9cbd6202e45ff2dd2196638710a2458f242e77b5
Duncan McRobert <dmcrobert@live.co.uk>
2020-04-19 12:17:09 -0400

    Added a jest config to set test environment, added blog_api tests using supertest and modified the logger to not log info in testing

---
+++ tests/blog_api.test.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are 3 blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(3)
+})
+
+test('the first blog is google', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].url).toBe('http://google.com')
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
