3e0a6026577142833601a9051248b86390b3bd94
Duncan McRobert <dmcrobert@live.co.uk>
2020-05-05 14:21:41 -0400

    Added JsonWebTokenError handler to middleware

--- utils/middleware.js
+++ utils/middleware.js
@@ -21,6 +21,13 @@ const errorHandler = (error, req, res, next) => {
     else if (error.name === 'ValidationError') {
         return res.status(400).json({ error: error.message })
     }
+    else if (error.name === 'JsonWebTokenError') {
+        return res.status(401).json({
+            error: 'invalid token'
+        })
+    }
+
+    logger.error(error.message)
 
     next(error)
 }
