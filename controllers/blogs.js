b34ce782e8934f8b3e02dfbc339fc2445b6242ad
Duncan McRobert <dmcrobert@live.co.uk>
2020-05-11 18:08:31 -0400

    Added token authorization to blog posting extracted out into tokenExtractor helper method

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -3,14 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
-
 blogsRouter.get('/', async (req, res, next) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     res.json(blogs.map(blog => blog.toJSON()))
@@ -18,9 +10,9 @@ blogsRouter.get('/', async (req, res, next) => {
 
 blogsRouter.post('/', async (req, res, next) => {
     const body = req.body
-    const token = getTokenFrom(req)
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(req.token, process.env.SECRET)
+
+    if (!req.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid'})
     }
     const user = await User.findById(decodedToken.id)
